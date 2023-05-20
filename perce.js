267a420713f2d37c828ad5621376dc24d23d794e
MsPwesh <ogwepreciouso@gmail.com>
2023-05-20 14:52:49 +0100

    percentage calculator

---
+++ perce.js
@@ -0,0 +1,10 @@
+// function percentage(){
+//     // var amount= document.getElementById("amount")
+// var percent = document.getElementById("percent")
+// var num = document.getElementById("num")
+// var result= document.getElementById("result")
+
+// calculate.addEventEventListener('click', (e) => {
+//   e.preventDefault();
+//   result.value = (amount.value*percentage.value)/100;
+// })
